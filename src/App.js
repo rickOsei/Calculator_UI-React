a47014f2ffcff96691ed82299eef643829935acd
Osei Rick <rickoseiampah@gmail.com>
2022-06-22 16:09:46 +0300

    new changes

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container text-center box">
+      <div class="row firstrow">
+        <div class="col-lg-12 col0 text-right">
+          <p>0</p>
+        </div>
+      </div>
+      <div class="row secondrow">
+        <div class="col-lg-3 col1">
+          <p>AC</p>
+        </div>
+        <div class="col-lg-3 col2">
+          <p>+/-</p>
+        </div>
+        <div class="col-lg-3 col3">
+          <p>%</p>
+        </div>
+        <div class="col-lg-3 col4">
+          <p>÷</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col1">
+          <p>7</p>
+        </div>
+        <div class="col-lg-3 col2">
+          <p>8</p>
+        </div>
+        <div class="col-lg-3 col3">
+          <p>9</p>
+        </div>
+        <div class="col-lg-3 col4">
+          <p>x</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col1">
+          <p>4</p>
+        </div>
+        <div class="col-lg-3 col2">
+          <p>5</p>
+        </div>
+        <div class="col-lg-3 col3">
+          <p>6</p>
+        </div>
+        <div class="col-lg-3 col4">
+          <p>-</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col1">
+          <p>1</p>
+        </div>
+        <div class="col-lg-3 col2">
+          <p>2</p>
+        </div>
+        <div class="col-lg-3 col3">
+          <p>3</p>
+        </div>
+        <div class="col-lg-3 col4">
+          <p>+</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6 col1">
+          <p>0</p>
+        </div>
+        <div class="col-lg-3 col3">
+          <p>.</p>
+        </div>
+        <div class="col-lg-3 col4">
+          <p>=</p>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
